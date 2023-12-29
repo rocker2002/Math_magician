74ecf1642d58b42fa40764c0a2dc40d20bf42c98
rocker2002 <magicalmoizz@gmail.com>
2023-12-29 19:34:16 +0500

    Add: Calculator, Navbar, Quote

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Calculator from './components/Calculator';
+import Quote from './components/Quote'; 
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container mt-3">
+        <Routes>
+        <Route path="/" exact component={<App/>} />
+        <Route path="/calculator" component={<Calculator/>} />
+        <Route path="/quote" component={<Quote/>} />
+      </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
